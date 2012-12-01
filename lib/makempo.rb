0a40448d08beae77819b6cba12fe37f7df74b9a6
labocho <labocho@penguinlab.jp>
2012-12-02 01:25:49 +0900

    move animation-gif related code to lib

--- lib/makempo.rb
+++ lib/makempo.rb
@@ -1,4 +1,7 @@
 require "makempo/version"
+require "shellwords"
+require "tmpdir"
+require "fileutils"
 
 module Makempo
   require "makempo/app2"
@@ -17,5 +20,29 @@ module Makempo
     left.app2.mp_index_ifd.indivisual_image_data_offset_2 = left.file_size - left.mp_header_pos
 
     left.to_s + right.to_s
+  end
+
+  def from_animation_gif(path)
+    mpo = nil
+    Dir.mktmpdir do |dir|
+      # agif.gif -> splitted.gif.0 splitted.gif.1
+      system "convert +adjoin #{path.shellescape} #{File.join(dir, "splitted.gif").shellescape}"
+
+      # splitted.gif.0 -> l.gif
+      # splitted.gif.1 -> r.gif
+      lgif = File.join(dir, "splitted-0.gif")
+      rgif = File.join(dir, "splitted-1.gif")
+
+      # l.gif -> l.jpg
+      # r.gif -> r.jpg
+      ljpg = File.join(dir, "l.jpg")
+      rjpg = File.join(dir, "r.jpg")
+      system Shellwords.join(["convert", lgif, ljpg])
+      system Shellwords.join(["convert", rgif, rjpg])
+
+      # l.jpg, r.jpg -> out.mpo
+      mpo = Makempo.from_files(ljpg, rjpg)
+    end
+    mpo
   end
 end
